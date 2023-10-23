422261ca0547c8fdbcc4856aa374bb61bb0f42d3
unknown <TaherSalah>
2023-10-23 19:44:31 +0300

    add spinner and passing data

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return (
       <>
-        <div className="bg-dark text-warning fixed-bottom bottom-0 end-0 text-center">
+        <div className="bg-dark text-warning fixed-bottom bottom-0 end-0 text-center ">
           <h1>Footer</h1>
         </div>
       </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { Component } from 'react'
 import style from './Home.module.css'
+import { Link } from 'react-router-dom';
+import imgJif from '../../images/loading-gif.gif'
 export default class Home extends Component {
     state = {
         recipes: []
@@ -19,17 +21,21 @@ export default class Home extends Component {
         return (
             <>
                 <div className='container'>
-                    <div className="row">
+                    {this.state.recipes.length > 0 ? <div className="row">
                         {this.state.recipes.map((recipe) =>
-                        
+
                             <div className='col-md-4'>
-                                <div className="data">
-                                    <h5>{recipe.title}</h5>
+                                <div className="data text-center my-2">
                                     <img src={recipe.image_url} className={`${style.imgHeight} w-100`} alt="" srcset="" />
+                                    <h6>{recipe.title}</h6>
+                                    <Link to={recipe.publisher_url} target='_blank'>publisher</Link>
                                 </div>
                             </div>
                         )}
-                    </div>
+                    </div> : <div className=' min-vh-100  d-flex justify-content-center   '>
+                        <img src={imgJif} alt="" srcset="" className='min-vh-25 w-25' />
+                        </div>}
+
                 </div>
             </>
         )
